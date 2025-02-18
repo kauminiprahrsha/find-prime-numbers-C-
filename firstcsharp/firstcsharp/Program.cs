584f0b452c159ae3c2848219abe4663da9f1b386
kauminiprahrsha <148688303+kauminiprahrsha@users.noreply.github.com>
2025-02-18 15:13:36 +0530

    Add files via upload

---
+++ firstcsharp/firstcsharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace c__
+{
+
+    static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the number ");
+            int num;
+            num = Convert.ToInt32(Console.ReadLine());
+            int divisor = 0;
+
+
+            for(int i = 1; i <= num; i++)
+            {
+                if(num % i == 0)
+                {
+                    divisor++;
+                }
+            }
+
+
+            if (divisor == 2)
+            {
+                Console.WriteLine("The number you enter is a prime number ");
+            }
+
+            else
+            {
+                Console.WriteLine("The number you enter is a composite number");
+            } 
+        }
+    }
+}
+
